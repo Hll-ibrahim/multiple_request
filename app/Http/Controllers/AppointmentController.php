e0420f0aae2b75aec6905bcb8c452f1d4c3a1c3e
Halil Ibrahim Ozkul <ibrahimozkul@gmail.com>
2023-08-08 10:37:49 +0300

    Index page is done

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -13,7 +13,7 @@ class AppointmentController extends Controller
      */
     public function index()
     {
-        //
+        return view('appointment.index');
     }
 
     /**
--- resources/views/appointment/index.blade.php
+++ resources/views/appointment/index.blade.php
@@ -5,9 +5,42 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--JQuery-->
+    <script src="{{asset('jquery/jquery-3.6.0.js')}}"></script>
+
+    <!--bootstrap 5.2.3-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <title>Randevu</title>
 </head>
 <body>
+<div class="container">
+
+    <div class="row pt-4 pb-4" id="sessions">
+        <h4>Kontenjan Durumu</h4>
+        <div id="sessionContainer">
+            <!-- Sessions will be dynamically added here -->
+        </div>
+        <div class="mb-4 col-12 d-flex gap-4">
+            <div class="btn-group">
+                <button id="addSessionBtn" class="btn w-auto"
+                        style="padding: 8px 15px !important; font-size: 16px; background: linear-gradient(to right,#1e7a23,rgba(73,180,79,0.98)); color: #FFFFFF;">Kontenjan Ekle
+                </button>
+            </div>
+
+            <div class="btn-group">
+                <button id="removeSessionBtn" onclick="removeLastSession();" class="btn w-auto"
+                        style="padding: 8px 15px !important; font-size: 16px;background:linear-gradient(to right, #B71C1C,#f55555); color: #FFFFFF;">Kontenjan Kaldır
+                </button>
+            </div>
+            <div class="btn-group">
+                <button id="saveSessionBtn" onclick="postAcademician(sessionIndex);" class="btn w-auto"
+                        style="padding: 8px 15px !important; font-size: 16px;background:linear-gradient(to right, #01579B,#1986dc); color: #FFFFFF;">Kaydet
+                </button>
+            </div>
+
+        </div>
 
 
 
@@ -16,9 +49,176 @@
 
 
 
-<script>
 
-</script>
+
+        <script>
+
+            var sessions = [];
+            var sessionIndex = 0;
+            var saveSessionBtn = document.getElementById("saveSessionBtn");
+            var removeSessionBtn = document.getElementById("removeSessionBtn");
+
+            document.getElementById("addSessionBtn").addEventListener("click", function () {
+                var sessionContainer = document.getElementById("sessionContainer");
+                var newSession = createSession(sessionIndex);
+                sessionContainer.appendChild(newSession);
+                sessions.push({
+                    department_id: "",
+                    kontenjan: "",
+                    idHidden: ""
+                });
+
+                sessionIndex++;
+
+                if (sessionIndex > 0) {
+                    saveSessionBtn.style.display = "inline-block";
+                    removeSessionBtn.style.display = "inline-block";
+                }
+            });
+
+            if (sessionIndex === 0) {
+                saveSessionBtn.style.display = "none";
+                removeSessionBtn.style.display = "none";
+            }
+
+
+            function createSession(index) {
+                var session = document.createElement("div");
+                session.id = "session" + index;
+                var sessionForm = document.createElement("form");
+                sessionForm.id = "details_academician_modal" + index;
+                sessionForm.innerHTML = `
+    <div class="row mt-3 mb-4">
+        <div id="namestage" class="form-group mb-4 col-12 d-flex" style="gap:3rem;">
+            <div class="inp-group">
+                <label>Enstitü</label>
+                <select class="form-select" id="institute${index}" style="">
+                    <option selected disabled>Lütfen Seçiniz</option>
+
+                    <option value="91">FEN BİLİMLERİ</option>
+                    <option value="92">SOSYAL BİLİMLER</option>
+                    <option value="93">SAĞLIK BİLİMLERİ</option>
+                    <option value="94">EĞİTİM BİLİMLERİ</option>
+                </select>
+            </div>
+                <div class="inp-group">
+                    <label>Bilim Dalı</label>
+                    <select class="form-select" name="department_id" id="department_id${index}" style="width:475px;">
+
+                        <option selected>Lütfen önce enstitü seçiniz.</option>
+                        <option value=""></option>
+                    </select>
+                </div>
+                <div class="inp-group">
+                    <label>Kontenjan</label>
+                    <input class="form-control" type="number" name="kontenjan" id="kontenjan${index}">
+                </div>
+
+                <input type="hidden" id="idHidden${index}">
+            </div>
+        </div>`;
+
+                session.appendChild(sessionForm);
+
+                var instituteSelect = sessionForm.querySelector(`#institute${index}`);
+                instituteSelect.addEventListener("change", function () {
+                    var institute = this.value;
+                    var departmentSelect = sessionForm.querySelector(`#department_id${index}`);
+
+                    //var departments = ;
+                    var selectedInstituteDepartments = departments[institute];
+                    departmentSelect.innerHTML = "";
+
+                    selectedInstituteDepartments.forEach(department => {
+                        var option = document.createElement("option");
+                        option.value = department.id;
+                        option.textContent = department.name;
+                        departmentSelect.appendChild(option);
+                    });
+                });
+
+                return session;
+            }
+
+            function removeLastSession() {
+                if (sessions.length > 0) {
+                    var lastIndex = sessions.length - 1; // Son eklenen kontenjanın index değeri
+                    var sessionContainer = document.getElementById("sessionContainer");
+                    var sessionToRemove = document.getElementById("session" + lastIndex);
+                    sessionContainer.removeChild(sessionToRemove);
+                    sessions.splice(lastIndex, 1);
+                    sessionIndex--; // sessionIndex'i azaltarak index değerini düzeltiyoruz.
+                }
+
+                if (sessionIndex === 0) {
+                    saveSessionBtn.style.display = "none";
+                    removeSessionBtn.style.display = "none";
+                }
+            }
+
+
+            function postAcademician(count) {
+
+                var sessionCount = count - 1;
+                var sessionData = [];
+
+                for (var sessionIndex = 0; sessionIndex <= sessionCount; sessionIndex++) {
+                    console.log("index=" + sessionIndex)
+                    //var danismanId = ;
+                    var kontenjan = document.getElementById('kontenjan' + sessionIndex).value;
+                    var department_id = document.getElementById('department_id' + sessionIndex).value;
+
+                    sessionData.push({
+                        danismanId: 1,
+                        kontenjan: kontenjan,
+                        department_id: department_id,
+                    });
+                }
+                console.log(sessionData);
+
+                $.ajax({
+                    type: 'GET',
+                    url: ``,
+                    data: {
+                        sessions: sessionData,
+                    },
+                    headers: {'X-CSRF-TOKEN': "{{csrf_token()}}"},
+                    success: function (data) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Başarılı',
+                            text: 'Akademisyen Başarı ile Güncellendi',
+                            confirmButtonText: 'Tamam'
+                        })
+                        window.location.href = ''
+                    },
+                    error: function (data) {
+                        var errors = '';
+                        for (datas in data.responseJSON.errors) {
+                            errors += data.responseJSON.errors[datas] + '\n';
+                        }
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Başarısız',
+
+                            html: errors,
+                        });
+                    }
+
+                });
+            }
+        </script>
+
+
+
+
+
+
+        <script>
+
+        </script>
+
+    </div>
 
 </body>
 </html>
